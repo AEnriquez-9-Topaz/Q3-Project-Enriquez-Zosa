342146680814523a603a563c6544df885fccb314
AJ (Alejandro) <enriquez.alm@obmontessori.edu.ph>
2025-03-04 10:47:42 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,140 +1,86 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Q3-Project 1</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-	
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+function Personal (a, b) {
+						var a = document.getElementById('fullname').value;
+						window.alert ('We have accepted your submission, ' +  a + '!')
+					}
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+					function mouseOver(){
+						buy.style.background = "#C1121F";
+						buy.sytle.color = "ghostwhite"
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Newsreader:ital,opsz,wght@0,6..72,200..800;1,6..72,200..800&display=swap" rel="stylesheet">
+					}
 
-	<link href="https://fonts.googleapis.com/css2?family=Akaya+Kanadaka&family=Charm:wght@400;700&family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&family=Delius&family=Ephesis&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Lexend:wght@100..900&family=Lobster&family=Lora:ital,wght@0,400..700;1,400..700&family=Newsreader:ital,opsz,wght@0,6..72,200..800;1,6..72,200..800&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Rouge+Script&family=Sour+Gummy:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css2?family=Charm:wght@400;700&family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&family=Delius&family=Ephesis&family=Lexend:wght@100..900&family=Lobster&family=Lora:ital,wght@0,400..700;1,400..700&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Rouge+Script&family=Sour+Gummy:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-</head>
-<body>
-	<nav class="navbar navbar-expand-sm He cen col-sm-12">
-		<div class="container-fluid">
-		    <a class="navbar-brand col-sm-4" href=""><img src="https://i.ibb.co/NnvBgHh/Bites-removebg-preview.png" alt="logo"  class="d-inline-block align-top nega"></a>
-		    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
-		    	<span class="navbar-toggler-icon"></span>
-		    </button>
+					function mouseOut(){
+						buy.style.background = "#FFB703"
+						buy.style.color = "black"
+					}
 
-		    <div class="collapse navbar-collapse col-sm-4" id="collapsibleNavbar">
-		    	 <ul class="nav nav-tabs mx-auto" role="tablist">
-		       		<li class="nav-item mx-auto">
-	   							<a class="nav-link" href="index.html" >Home</a>
-	      			</li>
-		       		<li class="nav-item mx-auto">
-		         			 <a class="nav-link" href="schedule.html" >Transit Schedule</a>
-		        	</li>  
-		       		<li class="nav-item mx-auto">
-		          		<a class="nav-link" href="Contact.html">Contact Us</a>
-		       	 	</li>
-		      	</ul>
-		    </div>
+					// onchange js event
+					document.getElementById("fullname").onchange = function() {ChangeToCapital()}
+					function ChangeToCapital() {
+						var fname = document.getElementById("fullname");
+						fname.value = fname.value.toUpperCase();
+					}
 
-		    <div class="wrapper sc col-sm-4" >
-					<form action="/search" method="get">
-			    	<input type="search"  placeholder="Search.." name="query">
-			      <button type="submit">Submit</button>
-			    </form>
-	  		</div>
-	  	</div>
-	</nav>
-	<br /><br /><br />
-	
-	<div class="row">
-			<h1 class="Fancy"> Ticketing Center! </h1>
-			<hr /> <br />
-			<div class="Personal col-sm-4">
-			   <a href="https://ibb.co/ch4BHttx"><img src="https://i.ibb.co/xqxk0GG3/map.png" alt="map" border="0"></a><br /><a target='_blank' href='https://imgbb.com/'>forum image hosting</a><br />
-			</div>
+					function Price() {
+						var f = document.getElementById("from").value;
+						var t = document.getElementById("to").value;
+						var d = document.getElementById("option").value;
 
-			<div class="Ticket col-sm-4">
-				<details> 
-				   <summary>Ticket Price</summary>
-				   <em> Choose your ticket price </em>
-				   <hr />
-				   <form action="" placeholder="Seats">
-				   		<label> Leaving From: </label>
-					    <select name="from" id="from" class="col-sm-12 form-control">
-		 			    	<option disabled="true" selected="true" class="cen"> Locations </option>
-		   				   	<option value="Roosevelt">Roosevelt</option>
-		   		 			<option value="Balintawak">Balintawak</option>
-		    				<option value="Monumento">Monumento</option>
-		   				   	<option value="5th Avenue">5th Avenue</option>
-		   		 			<option value="R. Papa">R. Papa</option>
-		    				<option value="Abad Santos">Abad Santos</option>
-		   				   	<option value="Blumentritt">Blumentritt</option>
-		   		 			<option value="Tayuman">Tayuman</option>
-		    				<option value="Bambang">Bambang</option>
-		   				   	<option value="Doroteo Jose">Doroteo Jose</option>
-		   		 			<option value="Carriedo">Carriedo</option>
-		    				<option value="Central Terminal">Central Terminal</option>
-		   				   	<option value="United Nations">United Nations</option>
-		   		 			<option value="Pedro Gil">Pedro Gil</option>
-		    				<option value="Quirino">Quirino</option>
-		   				   	<option value="Vito Cruz">Vito Cruz</option>
-		   		 			<option value="Libertad">Libertad</option>
-		    				<option value="EDSA">EDSA</option>
-		    				<option value="Baclaran">Baclaran</option>
-	  					</select>
-	  					<br />
-	  					<label> Going to: </label>
-					    <select name="to" id="to" class="col-sm-12 form-control">
-		 			    	<option disabled="true" selected="true" class="cen"> Going to </option>
-		   				   	<option value="Roosevelt">Roosevelt</option>
-		   		 			<option value="Balintawak">Balintawak</option>
-		    				<option value="Monumento">Monumento</option>
-		   				   	<option value="5th Avenue">5th Avenue</option>
-		   		 			<option value="R. Papa">R. Papa</option>
-		    				<option value="Abad Santos">Abad Santos</option>
-		   				   	<option value="Blumentritt">Blumentritt</option>
-		   		 			<option value="Tayuman">Tayuman</option>
-		    				<option value="Bambang">Bambang</option>
-		   				   	<option value="Doroteo Jose">Doroteo Jose</option>
-		   		 			<option value="Carriedo">Carriedo</option>
-		    				<option value="Central Terminal">Central Terminal</option>
-		   				   	<option value="United Nations">United Nations</option>
-		   		 			<option value="Pedro Gil">Pedro Gil</option>
-		    				<option value="Quirino">Quirino</option>
-		   				   	<option value="Vito Cruz">Vito Cruz</option>
-		   		 			<option value="Libertad">Libertad</option>
-		    				<option value="EDSA">EDSA</option>
-		    				<option value="Baclaran">Baclaran</option>
-	  					</select>
-	  					<br />
-	  					<label> Options: </label>
-					    <select name="option" id="option" class="col-sm-12 form-control">
-		 			    	<option disabled="true" selected="true" class="cen"> Options </option>
-		   				   	<option value="None">None</option>
-		   		 			<option value="Student">Student</option>
-		    				<option value="Patron">Patron</option>
-	  					</select>
- 				   </form>
-				    <br />
-				    <button type="button" id="forprice" onclick="Price()"class="col-auto btn btn-warning btn-md"onmouseover="mouseOver()" onmouseout="mouseOut()"> Check Price </button>
-				    <br /> <br />
-				</details>
-			</div>
+						if (f === "Roosevelt") {Km1 = 20.7;}
+						if (f === "Balintawak") {Km1 = 19;}
+						if (f === "Monumento") {Km1 = 18;}
+						if (f === "5th Avenue") {Km1 = 17.7;}
+						if (f === "R. Papa") {Km1 = 16.4;}
+						if (f === "Abad Santos") {Km1 = 15;}
+						if (f === "5th Avenue") {Km1 = 14.1;}
+						if (f === "Blumentritt") {Km1 = 12.9;}
+						if (f === "Tayuman") {Km1 = 12;}
+						if (f === "Bambang") {Km1 = 11.4;}
+						if (f === "Doroteo Jose") {Km1 = 10.4;}
+						if (f === "Carriedo") {Km1 = 9;}
+						if (f === "Central Terminal") {Km1 = 8;}
+						if (f === "United Nations") {Km1 = 7;}
+						if (f === "Pedro Gil") {Km1 = 5.8;}
+						if (f === "Quirino") {Km1 = 4.6;}
+						if (f === "Vito Cruz") {Km1 = 3;}
+						if (f === "Libertad") {Km1 = 1.7;}
+						if (f === "EDSA") {Km1 = 0.9;}
+						if (f === "Baclaran") {Km1 = 0;}
 
-			<div class="Personal col-sm-4">
-				<h1> Your Price is: </h1>
-					<h3 id="price"> <i> Please buy a ticket</i></h3> 
-			</div>
-		</div>	
+						if (t === "Roosevelt") {Km2 = 20.7;}
+						if (t === "Balintawak") {Km2 = 19;}
+						if (t === "Monumento") {Km2 = 18;}
+						if (t === "5th Avenue") {Km2 = 17.7;}
+						if (t === "R. Papa") {Km2 = 16.4;}
+						if (t === "Abad Santos") {Km2 = 15;}
+						if (t === "5th Avenue") {Km2 = 14.1;}
+						if (t === "Blumentritt") {Km2 = 12.9;}
+						if (t === "Tayuman") {Km2 = 12;}
+						if (t === "Bambang") {Km2 = 11.4;}
+						if (t === "Doroteo Jose") {Km2 = 10.4;}
+						if (t === "Carriedo") {Km2 = 9;}
+						if (t === "Central Terminal") {Km2 = 8;}
+						if (t === "United Nations") {Km2 = 7;}
+						if (t === "Pedro Gil") {Km2 = 5.8;}
+						if (t === "Quirino") {Km2 = 4.6;}
+						if (t === "Vito Cruz") {Km2 = 3;}
+						if (t === "Libertad") {Km2 = 1.7;}
+						if (t === "EDSA") {Km2 = 0.9;}
+						if (t === "Baclaran") {Km2 = 0;}
+					
 
-	<br /><br /><br /><br />
-	<div class="footer">
-	<a href="https://github.com/AEnriquez-9-Topaz "> <p> <i> 2025 | Alejandro Enriquez & Ivy Zosa | 9-Topaz </i> </p> </a>
-	</div> 
-	<script src="main.js"></script>
-</body>
-</html>
+						if (d === "None") {Discount = 1;}
+						if (d === "Student") {Discount = 0.8;}
+						if (d === "Patron") {Discount = 0.7;}
+
+						var ans = Math.abs(Km1-Km2) * 3 * Discount
+						var r_ans =ans.toFixed(2)
+						document.getElementById('price').innerHTML=r_ans + ' Pesos' 	
+
+					}
+
+					function Buy() {
+						window.alert('Your ticket has been sent to you email!')
+					}
+
+
